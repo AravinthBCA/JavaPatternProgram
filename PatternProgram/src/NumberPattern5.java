825cb152b917348e5de136957633518e49ef9505
AravinthBCA <aravinthbca2001work@gmail.com>
2021-08-22 16:34:44 +0530

    Number Pattern5 Commit

---
+++ PatternProgram/src/NumberPattern5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NumberPattern5
+{
+    public static void main(String[] args)
+    {
+        // Create a new Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the number of rows from the user
+        System.out.println("Enter the number of rows to print the pattern ");
+
+        int rows = scanner.nextInt();
+        System.out.println("** Printing the pattern... **");
+        for (int i = rows-1; i >=0; i--) {
+			for (int j = 0; j < rows; j++) {
+				if(j%2==0) {
+					System.out.print((rows*j)+i+1+" ");
+				}
+				else {
+					System.out.print((rows*(j+1))-i+" ");
+				}
+			}
+			System.out.println();
+		}
+    }
+}
